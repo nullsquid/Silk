a97413708552f60a53f6836d5ba733c2031e68ba
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2016-11-23 11:04:00 -0500

    Split into correct nodes

---
+++ Assets/Silk/Parser/Parser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Silk
+{
+    public class Parser : MonoBehaviour
+    {
+        public TextAsset testText;
+
+        string textToParse;
+        //List<string> nodesToInterpret = new List<string>();
+        string[] nodesToInterpret;
+        string[] delim = new string[] { ":: " };
+        void Start()
+        {
+            textToParse = testText.text;
+            nodesToInterpret = textToParse.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        void BuildNodes(string newText)
+        {
+            //newNode = 
+        }
+        
+    }
+}
