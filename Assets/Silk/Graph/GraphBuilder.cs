fd695c51aec7d5a57f95fd3bc89bcf806d3e34e4
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-01-03 13:16:38 -0500

    Changed the NodeBuilder to be called GraphBuilder, Added skeleton of a SilkNode class

--- Assets/Silk/Graph/GraphBuilder.cs
+++ Assets/Silk/Graph/GraphBuilder.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Silk;
 namespace Silk
 {
-    public class NodeBuilder
+    public class GraphBuilder
     {
         //Silk.Graph graph = new Silk.Graph();
         LinkedList<Node> graph = new LinkedList<Node>();
@@ -25,8 +25,8 @@ namespace Silk
             node.Responses = nodeResponses;
             AddToGraph(node);
             Debug.Log(node);
-            
-            
+
+
         }
 
         public void BuildNode(string nodeKey, string nodePrompt, List<string> nodeResponses, List<string> nodeCustomTags)
---
+++ Assets/Silk/Graph/SilkNode.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SilkNode {
+
+    //Might use this to avoid naming conflicts instead of a generic Node datastructure
+
+	
+}
