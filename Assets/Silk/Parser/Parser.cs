7c92898f65dc34af8043c83d7fe62dbef710efac
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-06-07 08:47:26 -0400

    made mother public

--- Assets/Silk/Parser/Parser.cs
+++ Assets/Silk/Parser/Parser.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using Silk;
 namespace Silk
 {
+    //TODO Make Parser a Singleton
+    //TODO Change Parser's name to Silk or something that's more friendly
     public class Parser : MonoBehaviour
     {
         #region Public Variables
