b76c104c1159ecd9cd45b9fc185b432ad1fc7f6f
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-06-27 19:37:25 -0400

    Added CoroutineDataWrapper class to return data out of coroutines

---
+++ Assets/Silk/Silk Core/Utility/cdTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cdTest : MonoBehaviour {
+
+    private void Start() {
+        StartCoroutine(PrintWords());
+    }
+    IEnumerator PrintWords() {
+        CoroutineDataWrapper cd = new CoroutineDataWrapper(this, GetString());
+        yield return cd.coroutine;
+        Debug.Log("Result is " + cd.result);
+    }
+
+    IEnumerator GetString() {
+        yield return new WaitForSeconds(1.0f);
+        yield return Silk.Silky.Instance.mother.GetNodeByName("Sample","Start").silkLinks[0].LinkText;
+    }
+}
